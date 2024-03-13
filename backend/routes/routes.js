986564f2333d941dd60756ae974db0b0f209648c
PRAVIN PILODIYA <p143nimcet@gmail.com>
2024-03-13 12:16:40 +0530

    created: routes

---
+++ backend/routes/routes.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+const  router = express.Router();
+
+
+// route for getting all employees
+router.get('/employees',getAllEmployeeController);
+
+// route for adding new an employee
+router.post('/employees/add',addEmployeeController);
+//route for filter the employees
+router.get('/employees/filter',filterEmployeeController);;
+
+// route for searching the employees based on the name
+router.get('/employees/search',searchEmployeeController);
+
+// route for update an employee record
+router.put('/emplyees/:id',updateEmployeeController);
+
+// route for deleting an employee
+router.delete('/employees/:id',deleteEmployeeController);
+
+export default router;
