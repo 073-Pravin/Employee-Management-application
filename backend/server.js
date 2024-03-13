0de871ffe84b88726d460384a2193f4195098c3e
PRAVIN PILODIYA <p143nimcet@gmail.com>
2024-03-13 11:54:17 +0530

    created: server.js

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async(req,res)=>{
+    res.send('<h1>Welcome to the backend</h1>');
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
